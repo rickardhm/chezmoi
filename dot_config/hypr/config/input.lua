1373b57abac0339f64a2a1076afeaea015e0ea56
rickard hillebrink <rickard.hillebrink@gmail.com>
2026-08-05 14:04:35 +0200

    Update .config/hypr/config/input.lua

--- dot_config/hypr/config/input.lua
+++ dot_config/hypr/config/input.lua
@@ -6,13 +6,12 @@ h1.config ({
         kb_model = "",
         kb_options = "",
         kb_rules = "",
-
+        
         follow_mouse = 1,
-
-        sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
-
+        
+        sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
         touchpad {
             natural_scroll = false,
         },
-    }
+    },
 })
