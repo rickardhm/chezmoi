a077e2f15bcf558a6712f35cf187f832d1d220df
rickard hillebrink <rickard.hillebrink@gmail.com>
2026-08-05 15:50:30 +0200

    Update .config/hypr/config/keybindings.lua

--- dot_config/hypr/config/keybindings.lua
+++ dot_config/hypr/config/keybindings.lua
@@ -5,10 +5,10 @@
 local mainMod = "SUPER" -- Sets "Windows" key as main modifier
 
 -- Example binds, see https://wiki.hypr.land/Configuring/Basics/Binds/ for more
--- hl.bind(mainMod .. " + Q", hl.dsp.exec_cmd(terminal))
--- local closeWindowBind = hl.bind(mainMod .. " + C", hl.dsp.window.close())
+hl.bind(mainMod .. " + Q", hl.dsp.exec_cmd(terminal))
+local closeWindowBind = hl.bind(mainMod .. " + C", hl.dsp.window.close())
 -- closeWindowBind:set_enabled(false)
--- hl.bind(mainMod .. " + M", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
+hl.bind(mainMod .. " + M", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
 hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + F", hl.dsp.exec_cmd(fullscreen))
 hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
