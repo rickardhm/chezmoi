839f525dd1f0db15399f2b8c34c47b354fa14c0d
rickard hillebrink <rickard.hillebrink@gmail.com>
2026-08-05 14:18:22 +0200

    Update .config/hypr/config/keybindings.lua

--- dot_config/hypr/config/keybindings.lua
+++ dot_config/hypr/config/keybindings.lua
@@ -5,7 +5,7 @@
 local mainMod = "SUPER" -- Sets "Windows" key as main modifier
 
 -- Example binds, see https://wiki.hypr.land/Configuring/Basics/Binds/ for more
-hl.bind(mainMod .. " + return, hl.dsp.exec_cmd(terminal))
+hl.bind(mainMod .. " + return", hl.dsp.exec_cmd(terminal))
 local closeWindowBind = hl.bind(mainMod .. " + C", hl.dsp.window.close())
 -- closeWindowBind:set_enabled(false)
 hl.bind(mainMod .. " + M", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
